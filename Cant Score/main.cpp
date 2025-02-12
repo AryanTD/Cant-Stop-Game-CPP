4d2dd26b0f787b9fcd5bc2ea7f8413af240466b1
AryanTD <aryantandon285@gmail.com>
2025-02-12 15:46:21 -0500

    Initial Commit

---
+++ Cant Score/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cant Score
+//
+//  Created by Aryan Tandon on 2/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
